fb9e7c2dc0a9b6f573b7a750c1c13cd7779daea9
TeodorioNeto <83874983+TeodorioNeto@users.noreply.github.com>
2021-05-30 17:43:59 -0400

    Add files via upload

---
+++ ConversoDeFparaC2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ConversoDeFparaC{
+    public static void main(String args[]) {
+	
+	double temperaturaC,temperaturaF;
+	
+	temperaturaF= 100;
+	
+	temperaturaC=((temperaturaF-32)/1.8);
+	
+	System.out.println(temperaturaF+ " graus Fahrenheit = "+ temperaturaC +" graus Celsius");
+}
+    }
