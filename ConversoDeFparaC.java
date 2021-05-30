f135b80fcaed8787465ac18e09df911b4e775ce0
TeodorioNeto <83874983+TeodorioNeto@users.noreply.github.com>
2021-05-30 15:39:16 -0400

    Add files via upload

---
+++ ConversoDeFparaC.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]) {
+	
+	double temperaturaC,temperaturaF;
+	
+	Scanner s = new Scanner(System.in);
+	
+	System.out.println("Insira a temperatura em Fahrenheit:\n");
+	
+	 temperaturaF= s.nextDouble();
+	
+	temperaturaC=((temperaturaF-32)/1.8);
+	
+	System.out.println(temperaturaF+ " graus Fahrenheit = "+ temperaturaC +" graus Celsius");
+}
+    }
